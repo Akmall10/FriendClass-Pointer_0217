df726a19e44091a9428fd27b520e7ef779b76a1e
Akmall10 <akmalprasety53@gmail.com>
2026-04-27 17:46:40 +0700

    feat: add class mahasiswa with nim attribute

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -1,8 +1,7 @@
+#include <iostream>
+using namespace std;
 
 
-class mahasiswa {
-    public:
-        int nim;
         void showNim() {
             cout << "No Induk = " << nim << endl;
         }
