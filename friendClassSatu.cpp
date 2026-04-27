c2319102d34a0ba14fbad8aebca5ce0d05497dfe
Akmall10 <akmalprasety53@gmail.com>
2026-04-27 18:13:21 +0700

    feat: add class siswa with setId and displayAll

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,12 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
-
-class siswa {
+class siswa;
+class orang {
     private:
-        int id;
+        string nama;
     public:
-        void setId(int pId);
-        void displayAll(orang &a);
+        void setNama(string pNama);
+        friend class siswa;
 };
+
+
 
 void siswa::displayAll(orang &a) {
     cout << id << endl << a.nama;
