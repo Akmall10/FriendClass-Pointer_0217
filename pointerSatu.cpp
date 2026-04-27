0a3f26246059677731778a02c3be62d88d04798d
Akmall10 <akmalprasety53@gmail.com>
2026-04-27 17:50:13 +0700

    feat: add pointer reference refMhs

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -9,11 +9,11 @@ class mahasiswa {
         }
 };
 
+int main() {
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim();    // Member Access Operator
 
-
-    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
-    refMhs.nim = 2;           // Member Access Operator
-    mhs.showNim();
+   
 
     mahasiswa *pMhs = &mhs;  // Pointer Dereference pMhs
     pMhs->nim = 3;            // Arrow Operator
