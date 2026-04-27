1bdd3b3e1c50f49257cf01cfc79213a649c8bd89
Akmall10 <akmalprasety53@gmail.com>
2026-04-27 18:12:50 +0700

    feat: add class orang with friend class siswa declaration

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,15 +1,4 @@
-#include <iostream>
-#include <string>
-using namespace std;
 
-class siswa;
-class orang {
-    private:
-        string nama;
-    public:
-        void setNama(string pNama);
-        friend class siswa;
-};
 
 class siswa {
     private:
